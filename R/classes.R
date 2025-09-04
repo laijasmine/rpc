4b8df8abd6e3dc269397691d5b07dad68e6abc05
Jasmine <jsminelai@gmail.com>
2025-09-03 20:58:25 -0700

    setup package

---
+++ R/classes.R
@@ -0,0 +1,12 @@
+# @param start_date description
+# @return data.frame containing dates, sessions, exclusions
+create_class <- function(start_date, end_date, class) {
+  
+}
+
+get_holidays <- function(province) {
+
+}
+
+
+
