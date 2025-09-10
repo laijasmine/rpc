96769a5bc898b625a5efebed909edc915429bad4
Jasmine <13112379+laijasmine@users.noreply.github.com>
2025-09-09 23:44:03 -0700

    setup contracts

--- inst/setup.R
+++ inst/setup.R
@@ -44,3 +44,58 @@ sheet_write(
   ss = ssid,
   sheet = session
 )
+
+## Contracts
+library(purrr)
+library(quarto)
+library(rpc)
+library(glue)
+
+year         <- 2025
+session      <- "winter"
+hourly_rate  <- 36
+class_length <- 3
+class_prep   <- 3
+class_schedule <- get_class_schedule()
+instructors <- unique(class_schedule$instructor)
+
+# per instructor
+i <- "David Liu"
+instructor_classes <- class_schedule |>
+  filter(instructor == {{ i }}, class_type == "public") |>
+  select(-class_type, -instructor, -cost)
+
+number_of_courses <- NROW(instructor_classes)
+kiln <- ifelse(
+  i == "David Liu",
+  "+ 2 hours kiln loading per course",
+  ""
+)
+kiln_hours <- ifelse(i == "David Liu", 2, 0) * number_of_courses
+
+class_prep <- class_prep_hours * number_of_courses
+total_sessions <- sum(as.numeric(instructor_classes$sessions))
+total_class_time <- (class_length * total_sessions) +
+  class_prep +
+  kiln_hours
+total_contract_amount <- hourly_rate * total_class_time
+
+quarto_render(
+  input = glue("{getwd()}/inst/contract_template.qmd"),
+  output_file = glue("{year}_{session}_{i}.pdf"),
+  output_format = "pdf",
+  execute_dir = getwd(),
+  execute_params = list(
+    instructor = i,
+    day = instructor_classes$day,
+    sessions = instructor_classes$sessions,
+    class_time = instructor_classes$class_time,
+    start_date = as.character(instructor_classes$start_date),
+    end_date = as.character(instructor_classes$end_date) #,
+    #exclusions = instructor_classes$exclusions,
+    class_prep = class_prep,
+    total_sessions = total_sessions,
+    total_class_time = total_class_time,
+    total_contract_amount = total_contract_amount
+  )
+)
